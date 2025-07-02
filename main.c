7e6f2750dc061c35eb85e3af474d449f1f0bfcba
Toutl <ubaldodiego06@gmail.com>
2025-07-02 15:28:55 -0600

    Testing norm and normalize

--- main.c
+++ main.c
@@ -5,13 +5,14 @@
 int
 main(void)
 {
-  Vector *u, *v, *w;
+  Vector *u, *v, *w, *z;
   float a = 2.0f;
   size_t n = 3;
 
   u = vec_create(n);
   v = vec_create(n);
   w = vec_create(n);
+  z = vec_create(n);
 
   u->data[0] = 3.0f;
   u->data[1] = 5.7f;
@@ -21,7 +22,16 @@ main(void)
   vec_print(v);
 
   w = vec_scale(vec_add(u, v), a);
+  printf("Scaled added: ");
   vec_print(w);
+
+  z = vec_normalize(w);
+  printf("Norm %.2f\n", vec_norm(w));
+  printf("Normalized: ");
+  vec_print(z);
+
+  a = vec_norm(z);
+  printf("Norm: %.2f\n", a);
 
   veclist_free();
   return 0;
